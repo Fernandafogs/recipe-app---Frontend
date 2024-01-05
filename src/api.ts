53f9eed408987f5de2e04f7e96229557fe1a45f4
Fernanda Fogaca <fernanda.ramos.br@gmail.com>
2024-01-05 11:53:41 +0000

    alterando api

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import { Recipe } from "./types";
 
 export const searchRecipes = async (searchTerm: string, page: number) => {
-    const baseUrl = new URL ("https://recipe-app-backend-sigma.vercel.app/api/recipes/search");
+    const baseUrl = new URL ("https://recipe-app-backend-psi.vercel.app/api/recipes/search");
     baseUrl.searchParams.append("searchTerm", searchTerm);
     baseUrl.searchParams.append("page", String(page));
 
@@ -14,7 +14,7 @@ export const searchRecipes = async (searchTerm: string, page: number) => {
 }
 
 export const getRecipeSummary = async (recipeId: string) => {
-    const url = new URL (`https://recipe-app-backend-sigma.vercel.app/api/recipes/${recipeId}/summary`)
+    const url = new URL (`https://recipe-app-backend-psi.vercel.app/recipes/${recipeId}/summary`)
     const response = await fetch(url)   
 
     if (!response.ok) {
@@ -24,7 +24,7 @@ export const getRecipeSummary = async (recipeId: string) => {
 };
 
 export const getFavouriteRecipes =async () => {
-    const url = new URL ('https://recipe-app-backend-sigma.vercel.app/api/recipes/favourite')
+    const url = new URL ('https://recipe-app-backend-psi.vercel.app/api/recipes/favourite')
     const response = await fetch(url);
 
     if (!response.ok) {
@@ -34,7 +34,7 @@ export const getFavouriteRecipes =async () => {
 }
 
 export const addFavouriteRecipe = async (recipe: Recipe) => {
-    const url = new URL ('https://recipe-app-backend-sigma.vercel.app/api/recipes/favourite');
+    const url = new URL ('https://recipe-app-backend-psi.vercel.app/api/recipes/favourite');
     const body = {
         recipeId: recipe.id
     }
@@ -53,7 +53,7 @@ export const addFavouriteRecipe = async (recipe: Recipe) => {
 }
 
 export const removeFavouriteRecipe = async (recipe: Recipe) => {
-    const url = new URL ('https://recipe-app-backend-sigma.vercel.app/api/recipes/favourite');
+    const url = new URL ('https://recipe-app-backend-psi.vercel.app/api/recipes/favourite');
     const body = {
         recipeId: recipe.id
     }
